8a0ce3da940175f1f8b4fc8ebd43cde3af0cc24c
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-05-07 07:08:58 +0200

    Edit field

--- src/main/java/org/k8s/searchservice/entity/UserDTO.java
+++ src/main/java/org/k8s/searchservice/entity/UserDTO.java
@@ -4,9 +4,9 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 
-import static org.springframework.data.elasticsearch.annotations.FieldType.Text;
+import static org.springframework.data.elasticsearch.annotations.FieldType.Search_As_You_Type;
 
 @Document(indexName = "user_index")
-public record UserDTO(@Field(type = Text) String name, String imageLink, @Id String userID) {
+public record UserDTO(@Field(type = Search_As_You_Type) String name, @Id String userID) {
 }
 //Using same data as UserService-repo in chatgut
