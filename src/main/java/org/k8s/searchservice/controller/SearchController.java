b5da9eab8883441d5f09b68a41a436da0631860d
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-05-07 14:46:45 +0200

    Add restclient to fetch new users

--- src/main/java/org/k8s/searchservice/controller/SearchController.java
+++ src/main/java/org/k8s/searchservice/controller/SearchController.java
@@ -1,34 +1,47 @@
 package org.k8s.searchservice.controller;
 
 
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.k8s.searchservice.entity.UserDTO;
 import org.k8s.searchservice.repository.SearchRepository;
 import org.k8s.searchservice.service.SearchService;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClient;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @CrossOrigin
 public class SearchController {
 
     private final SearchService searchService;
+    private final RestClient restClient;
 
     //Use this to persist and make operations on entities. Acts as / is a db.
     private final ElasticsearchOperations elasticsearchOperations;
     private final SearchRepository searchRepository;
 
-    public SearchController(SearchService searchService, ElasticsearchOperations elasticsearchOperations, SearchRepository searchRepository) {
+    public SearchController(SearchService searchService, RestClient restClient, ElasticsearchOperations elasticsearchOperations, SearchRepository searchRepository, JacksonJsonpMapper jacksonJsonpMapper, ObjectMapper jacksonObjectMapper) {
         this.searchService = searchService;
+        this.restClient = restClient;
         this.elasticsearchOperations = elasticsearchOperations;
         this.searchRepository = searchRepository;
     }
 
-    @PostMapping("/newuser")
-    public ResponseEntity<String> updatedb(@RequestBody UserDTO user) {
-        elasticsearchOperations.save(user);
+    @GetMapping("/newuser")
+    public ResponseEntity<String> updatedb(){
+       var usersList = Objects.requireNonNull(restClient
+               .get()
+               .uri("http://usersapp:8002/users/all")
+               .retrieve()
+               .body(new ParameterizedTypeReference<List<UserDTO>>(){}));
+
+        elasticsearchOperations.save(usersList);
         return ResponseEntity.ok("Successfully updated");
     }
 
