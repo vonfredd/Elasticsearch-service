0fce61efb4123be2108cc79263a75fa19cd904b9
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-05-07 07:09:09 +0200

    Add basic security

---
+++ src/main/java/org/k8s/searchservice/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package org.k8s.searchservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/person/*","/newuser","/search").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable); // To be able to post (add), with postman
+        return http.build();
+    }
+}
