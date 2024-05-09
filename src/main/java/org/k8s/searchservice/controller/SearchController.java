aae456d896fc2234525404605f34b8c199f60cb4
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-05-09 20:07:48 +0200

    use ElasticSEarchOperations as best practice

--- src/main/java/org/k8s/searchservice/controller/SearchController.java
+++ src/main/java/org/k8s/searchservice/controller/SearchController.java
@@ -2,9 +2,13 @@ package org.k8s.searchservice.controller;
 
 import org.k8s.searchservice.config.Properties;
 import org.k8s.searchservice.entity.UserDTO;
-import org.k8s.searchservice.repository.SearchRepository;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.Criteria;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
+import org.springframework.data.elasticsearch.core.query.Query;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestClient;
@@ -18,13 +22,11 @@ public class SearchController {
 
     private final RestClient restClient;
     private final ElasticsearchOperations elasticsearchOperations;
-    private final SearchRepository searchRepository;
     private final Properties properties;
 
-    public SearchController(RestClient restClient, ElasticsearchOperations elasticsearchOperations, SearchRepository searchRepository, Properties properties) {
+    public SearchController(RestClient restClient, ElasticsearchOperations elasticsearchOperations, Properties properties) {
         this.restClient = restClient;
         this.elasticsearchOperations = elasticsearchOperations;
-        this.searchRepository = searchRepository;
         this.properties = properties;
     }
 
@@ -46,6 +48,27 @@ public class SearchController {
 
     @GetMapping("/search")
     public List<UserDTO> searchAsYouType(@RequestParam String query) {
-        return searchRepository.searchByName(query);
+        Criteria criteria = new Criteria("name")
+                .startsWith(query);
+        Query criteriaQuery = new CriteriaQuery(criteria);
+        SearchHits<UserDTO> searchHits = elasticsearchOperations.search(criteriaQuery, UserDTO.class);
+
+        return searchHits.getSearchHits()
+                .stream()
+                .map(SearchHit::getContent)
+                .toList();
+    }
+
+    @GetMapping("/search/contains")
+    public List<UserDTO> searchContainsAsYouType(@RequestParam String query) {
+        Criteria criteria = new Criteria("name")
+                .contains(query);
+        Query criteriaQuery = new CriteriaQuery(criteria);
+        SearchHits<UserDTO> searchHits = elasticsearchOperations.search(criteriaQuery, UserDTO.class);
+
+        return searchHits.getSearchHits()
+                .stream()
+                .map(SearchHit::getContent)
+                .toList();
     }
 }
